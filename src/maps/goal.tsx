5f1d8bd15d4006f9da5ebefa0460169a1b2d3c91
Michelle Tan <mtan123456789@gmail.com>
2020-05-03 04:06:05 -0400

    Added all the files and made it finite

---
+++ src/maps/goal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="goal" tilewidth="32" tileheight="32" tilecount="14" columns="14">
+ <image source="Pentagram.png" width="448" height="32"/>
+</tileset>
---
+++ src/maps/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="House" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Tileset_Larger.png" width="512" height="512"/>
+</tileset>
